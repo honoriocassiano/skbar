988241d5e9818b18f497ac5946039f3437897e6c
Cassiano Honorio da Silva <honoriocassiano@gmail.com>
2020-04-26 18:34:35 -0300

    The Begginng of a New Era

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,7 @@
-#include <igl/readOBJ.h>
 
-Eigen::MatrixXd V;
-Eigen::MatrixXi F;
+int main(int argc, char *argv[]) {
 
-int main(int argc, char *argv[])
-{
-
-    Eigen::MatrixXd V;
-    Eigen::MatrixXd F;
-
-    // Load a mesh in OFF format
-    igl::readOBJ("./models/3holes_quad.obj", V, F);
-
-    std::cout << F << std::endl;
+    // The Begginng of a New Era
 
     return 0;
 }
