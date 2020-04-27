a590b31260c790231e65d5004db15750d13fa560
Cassiano Honorio da Silva <honoriocassiano@gmail.com>
2020-04-26 21:13:45 -0300

    Update main

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,22 @@
+#include <OpenMesh/Core/IO/MeshIO.hh>
+
+#include "meshtypes.h"
+#include "patchtracer.h"
+
+using namespace skbar;
 
 int main(int argc, char *argv[]) {
 
     // The Begginng of a New Era
+    QuadMesh mesh;
+
+    if (!OpenMesh::IO::read_mesh(mesh, "/home/cassiano/workspace-cpp/skbar/models/plane.obj"))
+    {
+        std::cerr << "read error\n";
+        exit(1);
+    }
+
+    PatchTracer::Trace(mesh);
 
     return 0;
 }
