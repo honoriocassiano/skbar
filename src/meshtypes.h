5e36ee3c26cb624e6f40a874b0c4d951e5c4c361
Cassiano Honorio da Silva <honoriocassiano@gmail.com>
2020-04-26 21:06:20 -0300

    Add mesh types

---
+++ src/meshtypes.h
@@ -0,0 +1,35 @@
+//
+// Created by cassiano on 26/04/2020.
+//
+
+#ifndef SKBAR_MESHTYPES_H
+#define SKBAR_MESHTYPES_H
+
+#include <OpenMesh/Core/Mesh/TriMesh_ArrayKernelT.hh>
+#include <OpenMesh/Core/Mesh/PolyMesh_ArrayKernelT.hh>
+
+namespace skbar {
+// define traits
+    struct MyTraits : public OpenMesh::DefaultTraits {
+        // use double valued coordinates
+        typedef OpenMesh::Vec3d Point;
+        // use vertex normals and vertex colors
+        VertexAttributes(OpenMesh::Attributes::Normal |
+                         OpenMesh::Attributes::Color);
+        // store the previous halfedge
+        HalfedgeAttributes(OpenMesh::Attributes::PrevHalfedge);
+        // use face normals
+        FaceAttributes(OpenMesh::Attributes::Normal);
+        // store a face handle for each vertex
+//    FaceTraits
+//    {
+//        typename Base::Refs::FaceHandle my_face_handle;
+//    };
+    };
+// Select mesh type (TriMesh) and kernel (ArrayKernel)
+// and define my personal mesh type (MyMesh)
+    typedef OpenMesh::TriMesh_ArrayKernelT<MyTraits> TriMesh;
+    typedef OpenMesh::PolyMesh_ArrayKernelT<MyTraits> QuadMesh;
+}
+
+#endif //SKBAR_MESHTYPES_H
