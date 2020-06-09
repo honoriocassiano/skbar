fb7dd8549fc757e58c80bc56ad5287dff7115c64
cassiano <honoriocassiano@gmail.com>
2020-06-08 21:10:10 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,13 +6,12 @@
 #include "patchtracer.h"
 #include "patchquadrangulator.h"
 
-#include "lp_solve_5.5/lp_lib.h"
+#include "example.h"
 
 using namespace skbar;
 
 int main(int argc, char *argv[]) {
 
-    // The Begginng of a New Era
 //    QuadMesh mesh;
 //
 //    if (!OpenMesh::IO::read_mesh(mesh, "/home/cassiano/workspace-cpp/skbar/models/plane.obj")) {
@@ -22,84 +21,31 @@ int main(int argc, char *argv[]) {
 
     QuadMesh patch;
 
-    Eigen::VectorXi sides(4);
+    Eigen::VectorXi sides;
     std::vector<OpenMesh::Vec3d> positions;
 
-    std::vector<int> sidesVec{2, 2, 1, 1};
-//    std::vector<int> sidesVec{1, 1, 1, 1};
-
-    positions.emplace_back(0, 0, 0);
-    positions.emplace_back(0.5, 0, 0);
-    positions.emplace_back(1, 0, 0);
-    positions.emplace_back(1, 0.5, 0);
-    positions.emplace_back(1, 1, 0);
-//    positions.emplace_back(0.5, 1, 0);
-    positions.emplace_back(0, 1, 0);
-//    positions.emplace_back(0.5, 0, 0);
-//    positions.emplace_back(1, 0.5, 0);
-
-    // A simple quad
-    sides << 2, 2, 1, 1;
-//    sides << 1, 1, 1, 1;
-
+    generate_example_4_4(positions, sides);
+//    generate_example_4_1_complex(positions, sides);
+//    generate_example_4_2(positions, sides);
+//    generate_example_4_1(positions, sides);
+//    generate_example_5_1(positions, sides);
 
     auto param = PatchQuadrangulator::ComputeTopology(sides, patch);
 
-//    std::cout << std::distance(patch.vertices().begin(), patch.vertices().end());
-
-//    std::cout << param.l << "\n" << std::endl;
-
-//    std::cout << param.l << std::endl;
-//    std::cout << param.get_l_permuted() << std::endl;
-
-//    std::cout << param.get_l_permuted() << "\n" << std::endl;
-
-    auto shiftedPositions = PatchQuadrangulator::GetShiftedPositions(positions, param);
+    PatchQuadrangulator::SetLaplacianPositions(patch, positions);
 
     auto L = PatchQuadrangulator::GetLaplacianMatrix(patch, param);
-    auto b = PatchQuadrangulator::GetRightSide(patch, param, shiftedPositions);
-
-
-
-//    Log("%ld", patch.n_vertices());
-//    Log("%ld, %ld", L.rows(), L.cols());
-//    Log("%ld, %ld", b.rows(), b.cols());
-
-//    Log("%d", param.permutation.id);
-
-//    std::cout << L << "\n" << std::endl;
-//    std::cout << b << "\n" << std::endl;
+    auto b = PatchQuadrangulator::GetRightSide(patch, param);
 
     auto solver = new Eigen::SimplicialCholesky<Eigen::SparseMatrix<double>>(L.transpose() * L);
 
     Eigen::Matrix<double, -1, 3> x = solver->solve(L.transpose() * b);
-//    std::cout << x << std::endl;
 
     for (auto it = patch.vertices().begin(); it != patch.vertices().end(); it++) {
         patch.set_point(*it, OpenMesh::Vec3d(x(it->idx(), 0), x(it->idx(), 1), x(it->idx(), 2)));
     }
 
     OpenMesh::IO::write_mesh(patch, "/home/cassiano/retopo.obj");
-
-
-
-
-//    std::cout << laplacianMatrix << std::endl;
-
-//    Eigen::SimplicialCholesky<Eigen::MatrixXd> solver(L.reverse() * L);
-
-//    auto result = solver.solve(L.transpose() * b);
-
-
-//    auto nSides = sides.size();
-
-//    std::cout << param.permutation.id << std::endl;
-
-//    PatchTracer::Trace(mesh);
-
-//    PatchTracer::GetGrid(mesh, 0);
-
-
 
     return 0;
 }
