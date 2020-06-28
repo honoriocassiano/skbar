a3a3d6e8cfe2847071a22fe81d7c580f172c7721
cassiano <honoriocassiano@gmail.com>
2020-06-28 11:14:06 -0300

    Add docs to SDLApplication

--- src/sdlapplication.h
+++ src/sdlapplication.h
@@ -8,21 +8,52 @@ namespace skbar {
 class SDLApplication : public Application {
 
 public:
+
+    /**
+     * Constructor
+     *
+     * @param title title of window
+     * @param width width of window
+     * @param height height of window
+     */
     SDLApplication(const std::string &title, int width, int height);
 
     virtual ~SDLApplication();
 
+    /**
+     * Initialize all components (SDL, OpenGL, Camera...)
+     */
     void Initialize() override;
 
+    /**
+     * Load a mesh by filename
+     *
+     * @param filename name of model file
+     */
     void LoadMesh(const std::string &filename) override;
 
+    /**
+     * Save current mesh to .obj
+     *
+     * @param filename name to save te file
+     */
     void SaveMesh(const std::string &filename) override;
 
+    /**
+     * Run the application
+     */
     void Run();
 
 private:
+
+    /**
+     * Initialize SDL
+     */
     void InitializeSDL();
 
+    /**
+     * Initialize OpenGL
+     */
     void InitializeGL();
 
     /** Update states before render
@@ -38,14 +69,23 @@ private:
     void ProcessEvents();
 
 private:
+
+    // SDL window itself
     SDL_Window *window;
+
+    // OpenGL context of SDL window
     SDL_GLContext glContext;
 
+    // Process events not relative to the window
     EventProcessor eventProcessor;
 
+    // If application still running
     bool running;
 
+    // Window title
     const std::string title;
+
+    // Window dimensions
     int width;
     int height;
 };
