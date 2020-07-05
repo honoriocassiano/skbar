61d221825947380cacd8631ad80d546545db760a
cassiano <honoriocassiano@gmail.com>
2020-07-05 11:33:00 -0300

    Add abstractions to Quad and Tri mesh

---
+++ src/trimesh.h
@@ -0,0 +1,22 @@
+#ifndef TRIMESH_H
+#define TRIMESH_H
+
+#include "quadmesh.h"
+
+namespace skbar {
+
+class TriMesh {
+
+public:
+    TriMesh();
+    virtual ~TriMesh();
+
+    virtual void LoadMesh(const QuadMesh &mesh) = 0;
+
+    virtual std::size_t GetQuadFaceId(std::size_t triangleId) const = 0;
+
+    // TODO Add methods to manipulate curves
+};
+}
+
+#endif /* TRIMESH_H */
