5a9b152f1057b5f0806147b1b31252e846cdeb3d
cassiano <honoriocassiano@gmail.com>
2020-07-05 09:47:52 -0300

    Add vector operation functions

---
+++ src/glcamera.cpp
@@ -0,0 +1,120 @@
+#include "glcamera.h"
+
+#include <GL/glew.h>
+#include <GL/gl.h>
+
+#include <cmath>
+#include <algorithm>
+
+skbar::GLCamera::GLCamera(int _width, int _height)
+    : width(_width), height(_height), initialCenter{0, 0, 0}, initialPosition{0, 0, -1},
+      initialUp{0, 1, 0}, left{1, 0, 0}, center(initialCenter),
+      position(initialPosition), up(initialUp) {
+
+}
+
+skbar::GLCamera::~GLCamera() {
+#warning "Implement this"
+}
+
+void skbar::GLCamera::Resize(int width, int height) {
+#warning "Implement this"
+}
+
+void skbar::GLCamera::Rotate(float dtheta, float dphi) {
+	/**
+	 * Using formulas found <a href="http://mathworld.wolfram.com/SphericalCoordinates.html">here</a>
+	 * changing coordinate system (here = on Wolfram):
+	 *
+	 * x = y
+	 * y = z
+	 * z = x
+	 */
+    using namespace std;
+
+    // Direction vector from center to the camera
+    Vec3f dir {
+        position[0] - center[0],
+        position[1] - center[1],
+        position[2] - center[2]
+    };
+
+    // Invert rotation on x-window axis
+    float radius = Norm(dir);
+
+    // Calc new spherical coordinates
+    float theta = atan2(dir[0], dir[2]) - dtheta;
+    float phi = acos(dir[1] / radius) + dphi;
+
+    // Calc new position
+    float x = radius * sin(theta) * sin(phi);
+    float y = radius * cos(phi);
+    float z = radius * cos(theta) * sin(phi);
+
+    // Set new position
+    position[0] = center[0] + x;
+    position[1] = center[1] + y;
+    position[2] = center[2] + z;
+
+    // Calc new left
+    Vec3f tempLeft { -z, 0, x };
+
+    // Set new left
+    left = Normalize(tempLeft);
+
+    // Calc up vector
+    Vec3f tempUp = Cross(left, {x, y, z});
+
+    // Set new up vector
+    up = Normalize(tempUp);
+
+    // TODO Apply here?
+}
+
+void skbar::GLCamera::Drag(float dx, float dy) {
+
+    // Calc deltha
+    Vec3f v {
+        left[0]*dx - up[0]*dy,
+        left[1]*dx - up[1]*dy,
+        left[2]*dx - up[2]*dy
+   };
+
+    // Change position
+    position[0] += v[0];
+    position[1] += v[1];
+    position[2] += v[2];
+
+    // Change center
+    center[0] += v[0];
+    center[1] += v[1];
+    center[2] += v[2];
+}
+
+void skbar::GLCamera::Zoom(float zoom) {
+
+    Vec3f dir = Normalize(Vec3f{
+        center[0] - position[0],
+        center[1] - position[1],
+        center[2] - position[2],
+    });
+
+    // TODO CONTINUE HERE
+}
+
+
+void skbar::GLCamera::Apply() const {
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+
+    glViewport(0, 0, width, height);
+
+    gluPerspective(45, float(width) / (float)height, 0.01, 100);
+
+    gluLookAt(position[0], position[1], position[2],
+              center[0], center[1], center[2],
+              up[0], up[1], up[2]);
+
+    // TODO Apply here?
+}
---
+++ src/vector.h
@@ -0,0 +1,71 @@
+#ifndef VECTOR_UTILS_H_
+#define VECTOR_UTILS_H_
+
+#include <array>
+#include <algorithm>
+#include <numeric>
+
+namespace skbar {
+
+typedef std::array<int, 2> Vec2i;
+typedef std::array<float, 2> Vec2f;
+typedef std::array<int, 3> Vec3i;
+typedef std::array<float, 3> Vec3f;
+
+template<typename T, std::size_t N>
+inline constexpr T Norm(const std::array<T, N> &vec) {
+    T sum = std::accumulate(vec.begin(), vec.end(), (T) 0, [](T s, T n) { return n*n + s; });
+
+    return sum;
+}
+
+template<typename T, std::size_t N>
+constexpr std::array<T, N> Normalize(const std::array<T, N> &vec) {
+    T norm = Norm(vec);
+    std::array<T, N> normalized{0};
+
+    for (std::size_t i = 0; i < N; i++) {
+        normalized[i] = vec[i] / norm;
+    }
+
+    return normalized;
+}
+
+template<typename T>
+inline constexpr std::array<T, 3> Cross(const std::array<T, 3> &v1, const std::array<T, 3> &v2) {
+
+    return std::array<T, 3> {
+        v1[1]*v2[2] - v1[2]*v2[1],
+        v1[2]*v2[0] - v1[0]*v2[2],
+        v1[0]*v2[1] - v1[1]*v2[0]
+    };
+}
+
+template<typename T, std::size_t N>
+constexpr std::array<T, N> Dot(const std::array<T, N> &v1, const std::array<T, N> &v2) {
+    T sum = 0;
+
+    for (std::size_t i = 0; i < N; i++) {
+        sum += v1[i] * v2[i];
+    }
+
+    return sum;
+}
+
+}
+
+#ifdef VECTOR_SET_OPERATORS
+
+template<typename T>
+inline constexpr std::array<T, 3> operator^(const std::array<T, 3> &v1, const std::array<T, 3> &v2) {
+    return Cross(v1, v2);
+}
+
+template<typename T>
+inline constexpr std::array<T, N> operator*(const std::array<T, N> &v1, const std::array<T, N> &v2) {
+        return Dot(v1, v2);
+}
+
+#endif
+
+#endif
