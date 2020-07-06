dff8ceee8a94fbcd3e932af958ed816785853c98
cassiano <honoriocassiano@gmail.com>
2020-07-06 13:28:41 -0300

    Reset matrix mode after apply camera

--- src/glcamera.cpp
+++ src/glcamera.cpp
@@ -117,7 +117,7 @@ void skbar::GLCamera::Apply() const {
 
     glViewport(0, 0, width, height);
 
-    gluPerspective(45, float(width) / (float)height, 0.01, 100);
+    gluPerspective(45, float(width) / float(height), 0.01, 100);
 
     // TODO Check this
     glFrustum(bbox[0][0], bbox[0][1],
@@ -128,5 +128,6 @@ void skbar::GLCamera::Apply() const {
               center[0], center[1], center[2],
               up[0], up[1], up[2]);
 
+    glMatrixMode(GL_MODELVIEW);
     // TODO Apply here?
 }
