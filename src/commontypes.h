02ac36a265ddb967434499844904ca6ab9a70294
cassiano <honoriocassiano@gmail.com>
2020-07-07 15:19:06 -0300

    Move vector type definitions

--- src/commontypes.h
+++ src/commontypes.h
@@ -8,6 +8,11 @@ namespace skbar {
 // Bounding box to a 3D mesh
 typedef std::array<std::array<float, 2>, 3> BBox;
 
+typedef std::array<int, 2> Vec2i;
+typedef std::array<float, 2> Vec2f;
+typedef std::array<int, 3> Vec3i;
+typedef std::array<float, 3> Vec3f;
+
 }
 
 #endif
