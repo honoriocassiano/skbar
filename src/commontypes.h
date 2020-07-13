e7c3bbdf152f555ff3a5bd766267db6513401f8f
cassiano <honoriocassiano@gmail.com>
2020-07-13 15:33:23 -0300

    Add common type Line

--- src/commontypes.h
+++ src/commontypes.h
@@ -13,6 +13,24 @@ typedef std::array<float, 2> Vec2f;
 typedef std::array<int, 3> Vec3i;
 typedef std::array<float, 3> Vec3f;
 
+template <typename T>
+class Line {
+
+public:
+    Line(const T &_origin, const T &_direction) : origin(_origin), direction(_direction) {}
+
+    virtual ~Line() {}
+
+    const T &GetOrigin() const { return origin; }
+
+    const T &GetDirection() const { return direction; }
+
+private:
+    T origin;
+    T direction;
+
+};
+
 }
 
 #endif
