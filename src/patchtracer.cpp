74639c6ecbeb80d379dce3b9c58fec5c9a6e0285
cassiano <honoriocassiano@gmail.com>
2020-08-19 19:32:45 -0300

    Change function signatures and improve naming

--- src/patchtracer.cpp
+++ src/patchtracer.cpp
@@ -8,157 +8,155 @@
 
 #include "utils/debug.h"
 
-namespace skbar {
+void skbar::PatchTracer::Trace(skbar::OPQuadMesh &baseMesh) {
 
-    void PatchTracer::Trace(OPQuadMesh &baseMesh) {
+    auto mesh = baseMesh.Get();
 
-        auto mesh = baseMesh.Get();
+    std::set<int> edges;
 
-        std::set<std::pair<int, int>> edges;
+    auto singularities = FindSingularities(mesh);
 
-        auto singularities = FindSingularities(mesh);
+    for (const auto &v: singularities) {
+        const auto startHE = mesh.halfedge_handle(OpenMesh::SmartVertexHandle(v));
 
-        for (const auto &v: singularities) {
-            auto start = mesh.halfedge_handle(OpenMesh::SmartVertexHandle(v));
+        for (const auto itHE : startHE.from().outgoing_halfedges()) {
+            auto currentHE = itHE;
 
-            for (const auto &it : start.from().outgoing_halfedges()) {
-                auto current = it;
+            do {
 
-                do {
+//                auto pair = std::make_pair(std::min(currentHE.from().idx(), currentHE.to().idx()),
+//                                           std::max(currentHE.from().idx(), currentHE.to().idx()));
+                    const auto edge = currentHE.edge().idx();
 
-                    auto pair = std::make_pair(std::min(current.from().idx(), current.to().idx()),
-                                               std::max(current.from().idx(), current.to().idx()));
+                if (edges.find(edge) == edges.end()) {
+                    edges.insert(edge);
+                } else {
+                    // If already visited, skip
+                    break;
+                }
 
-                    if (edges.find(pair) == edges.end()) {
-                        edges.insert(pair);
-                    } else {
-                        // If already visited, skip
-                        break;
-                    }
-
-                    // auto p0 = mesh.point(OpenMesh::VertexHandle(pair.first));
-                    // auto p1 = mesh.point(OpenMesh::VertexHandle(pair.second));
+                // auto p0 = mesh.point(OpenMesh::VertexHandle(pair.first));
+                // auto p1 = mesh.point(OpenMesh::VertexHandle(pair.second));
 
 //                    Log("(%lf, %lf, %lf) -> (%lf, %lf, %lf)", p0[0], p0[1], p0[2], p1[0], p1[1], p1[2]);
 
-                    if (singularities.find(current.to().idx()) != singularities.end()) {
-                        break;
-                    }
+                if (std::binary_search(singularities.begin(), singularities.end(), currentHE.to().idx())) {
+                    break;
+                }
 
-                    // Just seek the border
-                    if (current.face().idx() == -1) {
-                        current = current.next();
-                    } else {
-                        current = current.next().opp().next();
-                    }
-                } while (true);
-            }
+                // Just seek the border
+                if (currentHE.face().idx() == -1) {
+                    currentHE = currentHE.next();
+                } else {
+                    currentHE = currentHE.next().opp().next();
+                }
+            } while (true);
         }
-    }
-
-    std::set<int> PatchTracer::FindSingularities(const OPQuadMesh::QuadMeshImpl &mesh, bool closed) {
-
-        std::set<int> singularities;
-
-        for (auto it = mesh.vertices().begin(); it != mesh.vertices().end(); it++) {
-
-            int i = std::distance(mesh.vertices().begin(), it);
-
-            if (closed) {
-
-                if (mesh.valence(OpenMesh::VertexHandle(i)) != 4) {
-                    singularities.insert(i);
-                }
-            } else {
-
-                if (mesh.valence(OpenMesh::VertexHandle(i)) < 3
-                    || mesh.valence(OpenMesh::VertexHandle(i)) > 4) {
-
-                    singularities.insert(i);
-                }
-            }
-
-
-        }
-
-//        printf("%d\n", j);
-        return singularities;
-    }
-
-    std::vector<std::vector<int>> PatchTracer::GetGrid(const OPQuadMesh &baseMesh, int patch) {
-
-        std::vector<std::vector<int>> grid;
-
-        auto mesh = baseMesh.Get();
-
-        // TODO Add patch tracing
-
-        // TODO Change parameter "closed"
-        auto singularities = FindSingularities(mesh, false);
-
-        auto v = *singularities.begin();
-
-        // TODO Set to half-edge inside the patch
-        auto start = mesh.halfedge_handle(OpenMesh::SmartVertexHandle(v));
-
-        auto firstInLine = start;
-
-        bool flipFirst = (firstInLine.face().idx() == -1);
-
-        do {
-            grid.push_back(FindLine(firstInLine, singularities));
-
-            if ((firstInLine.from().idx() != start.from().idx())
-                && (singularities.find(firstInLine.from().idx()) != singularities.end())) {
-                break;
-            }
-
-            auto p0 = mesh.point(OpenMesh::VertexHandle(firstInLine.from().idx()));
-            auto p1 = mesh.point(OpenMesh::VertexHandle(firstInLine.to().idx()));
-
-            Log("(%lf, %lf, %lf) -> (%lf, %lf, %lf)", p0[0], p0[1], p0[2], p1[0], p1[1], p1[2]);
-
-            if (flipFirst) {
-                firstInLine = firstInLine.opp().next().next();
-            } else {
-                firstInLine = firstInLine.next().next().opp();
-            }
-        } while (true);
-
-        return grid;
-    }
-
-    std::vector<int> PatchTracer::FindLine(const OpenMesh::SmartHalfedgeHandle &handle,
-                                           const std::set<int> &singularities) {
-        std::vector<int> line;
-
-        auto current = handle;
-
-        line.push_back(current.from().idx());
-
-        auto samePatch = true;
-
-        do {
-
-            line.push_back(current.to().idx());
-
-            // TODO Check if in another patch
-            if (singularities.find(current.to().idx()) != singularities.end()) {
-                break;
-            }
-
-            if (current.face().idx() != -1) {
-                if (current.next().opp().face().idx() == -1) {
-                    samePatch = false;
-                }
-
-                current = current.next().opp().next();
-            } else {
-                current = current.next();
-            }
-
-        } while (samePatch);
-
-        return line;
     }
 }
+
+std::vector<int> skbar::PatchTracer::FindSingularities(const skbar::OPQuadMesh::QuadMeshImpl &mesh, bool closed) {
+
+    // This vector is ordered by indexes, because of the iteration that will always place ordered
+    std::vector<int> singularities;
+
+    for (const auto it : mesh.vertices()) {
+
+        const auto i = it.idx();
+
+        if (closed) {
+
+            if (mesh.valence(OpenMesh::VertexHandle(i)) != 4) {
+                singularities.push_back(i);
+            }
+        } else {
+
+            if (mesh.valence(OpenMesh::VertexHandle(i)) < 3
+                || mesh.valence(OpenMesh::VertexHandle(i)) > 4) {
+
+                singularities.push_back(i);
+            }
+        }
+    }
+
+    return singularities;
+}
+
+std::vector<std::vector<int>> skbar::PatchTracer::GetGrid(const skbar::OPQuadMesh &baseMesh, int patch) {
+
+    std::vector<std::vector<int>> grid;
+
+    auto mesh = baseMesh.Get();
+
+    // TODO Add patch tracing
+
+    // TODO Change parameter "closed"
+    const auto singularities = FindSingularities(mesh, false);
+
+    const auto v = *singularities.begin();
+
+    // TODO Set to half-edge inside the patch
+    const auto startHE = mesh.halfedge_handle(OpenMesh::SmartVertexHandle(v));
+
+    auto firstInLineHE = startHE;
+
+    bool flipFirst = (firstInLineHE.face().idx() == -1);
+
+    do {
+        grid.push_back(FindLine(firstInLineHE, singularities));
+
+        if ((firstInLineHE.from().idx() != startHE.from().idx())
+            && (std::binary_search(singularities.begin(),
+                    singularities.end(), firstInLineHE.from().idx()))) {
+            break;
+        }
+
+        auto p0 = mesh.point(OpenMesh::VertexHandle(firstInLineHE.from().idx()));
+        auto p1 = mesh.point(OpenMesh::VertexHandle(firstInLineHE.to().idx()));
+
+//        Log("(%lf, %lf, %lf) -> (%lf, %lf, %lf)", p0[0], p0[1], p0[2], p1[0], p1[1], p1[2]);
+
+        if (flipFirst) {
+            firstInLineHE = firstInLineHE.opp().next().next();
+        } else {
+            firstInLineHE = firstInLineHE.next().next().opp();
+        }
+    } while (true);
+
+    return grid;
+}
+
+std::vector<int> skbar::PatchTracer::FindLine(const OpenMesh::SmartHalfedgeHandle &handle,
+                                              const std::vector<int> &singularities) {
+    std::vector<int> line;
+
+    auto current = handle;
+
+    line.push_back(current.from().idx());
+
+    auto samePatch = true;
+
+    do {
+
+        line.push_back(current.to().idx());
+
+        // TODO Check if in another patch
+        if (std::binary_search(singularities.begin(), singularities.end(), current.to().idx())) {
+            break;
+        }
+
+        if (current.face().idx() != -1) {
+            if (current.next().opp().face().idx() == -1) {
+                samePatch = false;
+            }
+
+            current = current.next().opp().next();
+        } else {
+            current = current.next();
+        }
+
+    } while (samePatch);
+
+    return line;
+}
+
