8d80c32e5a18fa8678bdda079ee14476e7754bbc
cassiano <honoriocassiano@gmail.com>
2020-08-20 18:54:11 -0300

    Remove useless comments

--- src/opmeshrenderer.cpp
+++ src/opmeshrenderer.cpp
@@ -147,31 +147,6 @@ void skbar::renderer::OPMeshRenderer::RenderSketch(const skbar::OPSketch &sketch
     }
 }
 
-//void skbar::renderer::RenderEdges(const skbar::OPTriMesh::TriMeshImpl &mesh) {
-//
-//    glHint(GL_LINE_SMOOTH_HINT, GL_NICEST);
-//    glEnable(GL_LINE_SMOOTH);
-//    glLineWidth(0.1f);
-//
-//    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-//
-//    glColor3f(0.3f, 0.3f, 0.3f);
-//
-//    glBegin(GL_LINES);
-//
-//    for (auto edge : mesh.edges()) {
-//        auto point1 = mesh.point(edge.v0());
-//        auto point2 = mesh.point(edge.v1());
-//
-//        glVertex3f(point1[0], point1[1], point1[2]);
-//        glVertex3f(point2[0], point2[1], point2[2]);
-//    }
-//
-//    glEnd();
-//
-//    glDisable(GL_LINE_SMOOTH);
-//}
-
 void skbar::renderer::OPMeshRenderer::RenderPatches(const skbar::OPQuadMesh::QuadMeshImpl &mesh) {
     glHint(GL_LINE_SMOOTH_HINT, GL_NICEST);
     glEnable(GL_LINE_SMOOTH);
