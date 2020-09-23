bc9444671efa62091e662e8a716619bc5203aa4e
cassiano <honoriocassiano@gmail.com>
2020-09-23 14:57:54 -0300

    Format file

--- src/sdlapplication.cpp
+++ src/sdlapplication.cpp
@@ -10,16 +10,16 @@
 
 // Event filter
 int RepeatedKeyEventFilter([[maybe_unused]] void *userdata, SDL_Event *event) {
-	if (event->type == SDL_KEYDOWN || event->type == SDL_KEYUP) {
-		// Return true if not repeated
-		return !event->key.repeat;
-	}
+    if (event->type == SDL_KEYDOWN || event->type == SDL_KEYUP) {
+        // Return true if not repeated
+        return !event->key.repeat;
+    }
 
-	return true;
+    return true;
 }
 
 skbar::SDLApplication::SDLApplication(const std::string &_title, int _width, int _height) :
-    title(_title), width(_width), height(_height) {
+        title(_title), width(_width), height(_height) {
 
     viewer = new Viewer(_width, _height);
 }
@@ -53,46 +53,46 @@ void skbar::SDLApplication::SaveMesh(const std::string &filename) {
 void skbar::SDLApplication::InitializeSDL() {
 
     if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-		throw std::runtime_error(std::string("Error: %s\n") + SDL_GetError());
-	}
+        throw std::runtime_error(std::string("Error: %s\n") + SDL_GetError());
+    }
 
 #if __APPLE__
-	// GL 3.2 Core + GLSL 150
-	const char* glsl_version = "#version 150";
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG);// Always required on Mac
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+    // GL 3.2 Core + GLSL 150
+    const char* glsl_version = "#version 150";
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG);// Always required on Mac
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
 #else
-	// GL 3.0 + GLSL 130
-	const char* glsl_version = "#version 130";
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK,
-			SDL_GL_CONTEXT_PROFILE_CORE);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+    // GL 3.0 + GLSL 130
+    const char *glsl_version = "#version 130";
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK,
+                        SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
 #endif
 
-	// Create window with graphics context
-	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
-	SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+    // Create window with graphics context
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+    SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
 
-     SDL_GL_SetAttribute(SDL_GL_MULTISAMPLEBUFFERS, 1);
-     SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, 2);
+    SDL_GL_SetAttribute(SDL_GL_MULTISAMPLEBUFFERS, 1);
+    SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, 2);
 
     SDL_WindowFlags window_flags = (SDL_WindowFlags) (SDL_WINDOW_OPENGL
-		| SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
-	window = SDL_CreateWindow(title.c_str(),
-	SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height,
-			window_flags);
+                                                      | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
+    window = SDL_CreateWindow(title.c_str(),
+                              SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height,
+                              window_flags);
 
-	glContext = SDL_GL_CreateContext(window);
-	SDL_GL_SetSwapInterval(1); // Enable vsync
+    glContext = SDL_GL_CreateContext(window);
+    SDL_GL_SetSwapInterval(1); // Enable vsync
 
-	SDL_SetEventFilter(RepeatedKeyEventFilter, nullptr);
+    SDL_SetEventFilter(RepeatedKeyEventFilter, nullptr);
 
-	SDL_GL_MakeCurrent(window, glContext);
+    SDL_GL_MakeCurrent(window, glContext);
 
     // SDL_SetHint( SDL_HINT_RENDER_SCALE_QUALITY, "1" );
 }
@@ -100,22 +100,22 @@ void skbar::SDLApplication::InitializeSDL() {
 void skbar::SDLApplication::InitializeGL() {
 
     if (glewInit() != GLEW_OK) {
-		throw std::runtime_error("Failed to initialize GLEW!");
-	}
+        throw std::runtime_error("Failed to initialize GLEW!");
+    }
 
-     glHint(GL_LINE_SMOOTH_HINT, GL_NICEST);
-     glHint(GL_POLYGON_SMOOTH_HINT, GL_NICEST);
+    glHint(GL_LINE_SMOOTH_HINT, GL_NICEST);
+    glHint(GL_POLYGON_SMOOTH_HINT, GL_NICEST);
 
-     glEnable(GL_MULTISAMPLE);
+    glEnable(GL_MULTISAMPLE);
 
-     glEnable(GL_LINE_SMOOTH);
-     glEnable(GL_POLYGON_SMOOTH);
+    glEnable(GL_LINE_SMOOTH);
+    glEnable(GL_POLYGON_SMOOTH);
 }
 
 void skbar::SDLApplication::Run() {
     running = true;
 
-	// SDL_MaximizeWindow(window);
+    // SDL_MaximizeWindow(window);
 
     while (running) {
         Update();
@@ -169,7 +169,7 @@ void skbar::SDLApplication::ProcessEvents() {
         }
 
         if (event.type == SDL_WINDOWEVENT
-                && (event.window.windowID == SDL_GetWindowID(window))) {
+            && (event.window.windowID == SDL_GetWindowID(window))) {
 
             switch (event.window.event) {
                 case SDL_WINDOWEVENT_CLOSE:
@@ -188,20 +188,20 @@ void skbar::SDLApplication::ProcessEvents() {
             }
         }
 
-		// Get all mouse events
-		switch (event.type) {
-			case SDL_MOUSEBUTTONDOWN:
-			case SDL_MOUSEBUTTONUP:
-			case SDL_MOUSEMOTION:
-			case SDL_MOUSEWHEEL:
-			case SDL_MOUSEWHEEL_FLIPPED:
-			case SDL_MOUSEWHEEL_NORMAL:
-			case SDL_FINGERDOWN:
-			case SDL_FINGERUP:
-			case SDL_FINGERMOTION:
+        // Get all mouse events
+        switch (event.type) {
+            case SDL_MOUSEBUTTONDOWN:
+            case SDL_MOUSEBUTTONUP:
+            case SDL_MOUSEMOTION:
+            case SDL_MOUSEWHEEL:
+            case SDL_MOUSEWHEEL_FLIPPED:
+            case SDL_MOUSEWHEEL_NORMAL:
+            case SDL_FINGERDOWN:
+            case SDL_FINGERUP:
+            case SDL_FINGERMOTION:
                 eventProcessor.ProcessMouse(event);
-				break;
-			}
+                break;
+        }
     }
 }
 
