ee4914134dd0e1367f7368f6f54285c2146489e1
cassiano <honoriocassiano@gmail.com>
2020-10-07 11:58:49 -0300

    Add new requadrangulator class

---
+++ src/requadrangulatornew.cpp
@@ -0,0 +1,26 @@
+//
+// Created by cassiano on 07/10/2020.
+//
+
+#include "requadrangulatornew.h"
+
+void skbar::RequadrangulatorNew::RequadrangulateAll(EditableMesh &mesh) {
+//    auto affectedPatches = FindAffectedPatches();
+//
+//    for (const auto &entry: affectedPatches) {
+//
+//        std::stringstream ss;
+//
+//        for (const auto &inOut: entry.second) {
+//            ss << "(" << inOut.in << ", " << inOut.out << ") ";
+//        }
+//
+//        std::string tuples = ss.str();
+//
+//        Log("%d: %s", entry.first, tuples.c_str());
+//    }
+//
+//    if (affectedPatches.size() > 1) {
+//        RequadrangulatePatches(editableMesh, affectedPatches);
+//    }
+}
---
+++ src/requadrangulatornew.h
@@ -0,0 +1,19 @@
+//
+// Created by cassiano on 07/10/2020.
+//
+
+#ifndef SKBAR_REQUADRANGULATORNEW_H
+#define SKBAR_REQUADRANGULATORNEW_H
+
+#include "editablemesh.h"
+
+namespace skbar {
+
+class RequadrangulatorNew {
+
+public:
+    static void RequadrangulateAll(EditableMesh &mesh);
+};
+}
+
+#endif //SKBAR_REQUADRANGULATORNEW_H
