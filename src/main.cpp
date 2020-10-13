b1619d211e9a56690b9bebd3cf3820f869e25379
cassiano <honoriocassiano@gmail.com>
2020-10-13 17:45:59 -0300

    Create test sketch

--- src/main.cpp
+++ src/main.cpp
@@ -20,6 +20,31 @@
 
 using namespace skbar;
 
+struct SketchPoint {
+    Vec3f position;
+    bool isPatchEdge;
+    bool isEdge;
+
+    SketchPoint(const Vec3f &_position, bool _isPatchEdge, bool _isEdge)
+            : position(_position), isPatchEdge(_isPatchEdge), isEdge(_isEdge) {}
+};
+
+std::vector<SketchPoint> GetSketch() {
+
+    std::vector<SketchPoint> result;
+
+    result.emplace_back(Vec3f{0.0, 0.6, 0.0}, true, true);
+    result.emplace_back(Vec3f{0.3, 0.6, 0.0}, false, false);
+    result.emplace_back(Vec3f{0.36, 0.5, 0.0}, false, true);
+    result.emplace_back(Vec3f{0.43, 0.43, 0.0}, false, false);
+    result.emplace_back(Vec3f{0.5, 0.32, 0.0}, false, true);
+    result.emplace_back(Vec3f{0.57, 0.42, 0.0}, false, false);
+    result.emplace_back(Vec3f{0.69, 0.5, 0.0}, false, true);
+    result.emplace_back(Vec3f{1.0, 0.72, 0.0}, true, true);
+
+    return result;
+}
+
 int main(int argc, char *argv[]) {
 
 //    SDLApplication app("SkBaR", 1024, 768);
@@ -35,19 +60,8 @@ int main(int argc, char *argv[]) {
 
     mesh.Load("../models/parametric_plane.obj");
 
-     app.LoadMesh("/home/cassiano/Documents/workspace-cpp/skbar/models/3holes_quad.obj");
-     // app.LoadMesh("/home/cassiano/Documents/workspace-cpp/skbar/models/plane.obj");
+    auto sketch = GetSketch();
 
-     app.Run();
-
-//     EditableMesh mesh;
-//
-//     mesh.Load("../models/3holes_quad.obj");
-//
-//     auto [min, max] = mesh.GetBoundingBox();
-//
-//     std::cout << min << std::endl;
-//     std::cout << max << std::endl;
 
     return 0;
 }
