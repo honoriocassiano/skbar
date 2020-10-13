16ad2641164b86e941b58586c751f2167dac088b
cassiano <honoriocassiano@gmail.com>
2020-10-13 17:45:49 -0300

    Use test mesh

--- src/main.cpp
+++ src/main.cpp
@@ -22,26 +22,18 @@ using namespace skbar;
 
 int main(int argc, char *argv[]) {
 
-//    // Plane p(Vec3f(), Vec3f{0, 1, 0});
+//    SDLApplication app("SkBaR", 1024, 768);
 //
-//    // Line l(Vec3f{1, 1, 2}, Vec3f{1, 0, 0});
+//    app.Initialize();
 //
-//    Segment<Vec2f> s1(Vec2f{1, 1}, Vec2f{0, 1});
-//    Segment<Vec2f> s2(Vec2f(), Vec2f{1, 0});
+//    app.LoadMesh("/home/cassiano/Documents/workspace-cpp/skbar/models/3holes_quad.obj");
+//    // app.LoadMesh("/home/cassiano/Documents/workspace-cpp/skbar/models/plane.obj");
 //
-//    // Vec3f intersection;
-//    Vec2f intersection;
-//
-//    // bool hasIntersection = utils::CheckIntersectionPlaneLine(p, l, intersection);
-//
-//    bool hasIntersection = utils::CheckIntersectionSegmentSegment(s1, s2, intersection);
-//
-//    Log("%d: (%f, %f)", hasIntersection, intersection[0], intersection[1]);
-//    // Log("%d: (%f, %f, %f)", hasIntersection, intersection[0], intersection[1], intersection[2]);
+//    app.Run();
 
-     SDLApplication app("SkBaR", 1024, 768);
+    EditableMesh mesh;
 
-     app.Initialize();
+    mesh.Load("../models/parametric_plane.obj");
 
      app.LoadMesh("/home/cassiano/Documents/workspace-cpp/skbar/models/3holes_quad.obj");
      // app.LoadMesh("/home/cassiano/Documents/workspace-cpp/skbar/models/plane.obj");
