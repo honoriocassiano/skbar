fd2eed9f3d3e499dfd9af2b52994ce483b31f193
cassiano <honoriocassiano@gmail.com>
2020-10-19 21:00:31 -0300

    Make a simple quadrangulation

--- src/main.cpp
+++ src/main.cpp
@@ -23,9 +23,17 @@
 
 using namespace skbar;
 
-std::vector<Vec3f> GetSketch() {
+using VSketch = std::vector<Vec3f>;
 
-    std::vector<Vec3f> result;
+struct PolygonInfo {
+    std::map<std::pair<int, int>, OpenMesh::VertexHandle> parametricVertices;
+    int u_length;
+    int v_length;
+};
+
+VSketch GetSketch() {
+
+    VSketch result;
 
     result.push_back(Vec3f{0, 0.5, 0.0});
     result.push_back(Vec3f{0.2, 0.6, 0.0});
@@ -36,6 +44,48 @@ std::vector<Vec3f> GetSketch() {
     result.push_back(Vec3f{0.7, 0, 0.0});
 
     return result;
+}
+
+PolygonInfo GetPolygonInfo() {
+
+    const int u_length = 2;
+    const int v_length = 2;
+
+    std::map<std::pair<int, int>, OpenMesh::VertexHandle> parametricVertices;
+
+    parametricVertices.insert_or_assign({0, 0}, OpenMesh::VertexHandle(0));
+    parametricVertices.insert_or_assign({1, 0}, OpenMesh::VertexHandle(1));
+    parametricVertices.insert_or_assign({0, 1}, OpenMesh::VertexHandle(2));
+    parametricVertices.insert_or_assign({1, 1}, OpenMesh::VertexHandle(3));
+
+    return PolygonInfo{
+            parametricVertices,
+            u_length,
+            v_length
+    };
+}
+
+OPQuadMesh QuadrangulatePolygon1(const PolygonInfo &polygonInfo, const VSketch &sketch) {
+
+    Eigen::VectorXi sides;
+    std::vector<OpenMesh::Vec3d> positions;
+
+    // Fill sizes
+    sides.resize(5);
+    sides << 1, 1, 1, 6, 1;
+
+    {
+        // Fill positions
+        positions.emplace_back(1.0, 0.0, 0.0);
+        positions.emplace_back(1.0, 1.0, 0.0);
+        positions.emplace_back(0.0, 1.0, 0.0);
+
+        for (const auto &vertex: sketch) {
+            positions.emplace_back(vertex[0], vertex[1], vertex[2]);
+        }
+    }
+
+    return PatchQuadrangulator::Quadrangulate(sides, positions, false);
 }
 
 int main(int argc, char *argv[]) {
@@ -56,6 +106,9 @@ int main(int argc, char *argv[]) {
     const auto &quadmesh = dynamic_cast<const OPQuadMesh &>(mesh.GetQuad()).Get();
 
     auto sketch = GetSketch();
+    auto polygonInfo = GetPolygonInfo();
+
+    OPQuadMesh newPatch = QuadrangulatePolygon1(polygonInfo, sketch);
 
     return 0;
 }
